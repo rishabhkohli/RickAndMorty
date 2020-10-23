6b6ef7f3bb42c674ceaa56502240410bc5d45b63
Rishabh Kohli <rishabhkohli93@yahoo.com>
2020-10-23 19:56:39 -0400

    Set up MainActivity with NavController and use MainFragment

--- app/src/main/java/com/rishabhkohli/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/rishabhkohli/rickandmorty/MainActivity.kt
@@ -2,37 +2,14 @@ package com.rishabhkohli.rickandmorty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
-import com.rishabhkohli.rickandmorty.ui.RickAndMortyTheme
+import com.rishabhkohli.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            RickAndMortyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RickAndMortyTheme {
-        Greeting("Android")
-    }
+		val binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+	}
 }
